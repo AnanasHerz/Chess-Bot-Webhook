94d9afb7a94825ed41439434ca5654d21b2fe022
AnanasHerz <ilja.speicher@gmail.com>
2023-04-12 14:53:11 +0200

    Improved messaging

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ def loop():
         #Player
         player = 'ananasherz1' #Players username
         check = requests.get(f'https://api.chess.com/pub/player/{player}')
+        #Empty variables
+        rated = ''
+        time_class = ''
+        result = ''
+        reason = ''
+        side = ''
+        opponent = ''
+        endboard = ''
         if check.status_code == 200:
             archive = requests.get(f'https://api.chess.com/pub/player/ananasherz1/games/archives').json()
             newest_arc = int(len(archive['archives']))-1
@@ -33,104 +41,91 @@ def loop():
                     f.write(suspected_last_game)
                     f.close()
                     print('Url changed')
-                    message = True
+                    #Url var
+                    url = response['games'][newest]['url']
+                    #Rated or unrated
+                    if response['games'][newest]['rated'] is True:
+                        rated = 'a rated'
+                    if response['games'][newest]['rated'] is False:
+                        rated = 'an unrated'
+                    else:
+                        pass
+                    #Time class of game (Blitz, Bullet...) and url friendly endboard image adress
+                    time_class = response['games'][newest]['time_class']
+                    fen = response['games'][newest]['fen'].replace(" ", "%20")
+                    #Defines the sides
+                    if response['games'][newest]['white']['username'] == player:
+                        side = response['games'][newest]['white']
+                        opponent = response['games'][newest]['black']
+                    if response['games'][newest]['black']['username'] == player:
+                        side = response['games'][newest]['black']
+                        opponent = response['games'][newest]['white']
+                    if response['games'][newest]['white']['username'] != player and response['games'][newest]['black']['username'] != player:
+                        side = 'invalid colors'
+                        opponent = 'invalid colors'
+                        print('Invalid color')
+                    else:
+                        pass
+                    #Result codes
+                    if side['result'] == 'win':
+                        result = 'won'
+                    if opponent['result'] == 'win':
+                        result = 'lost'
+                    if side['result'] == 'agreed' or side['result'] == 'repetition' or side['result'] == 'stalemate' or side['result'] == 'insufficient' or side['result'] == '50move' or side['result'] == 'timevsinsufficient':
+                        result = 'drawed'
+                    else:
+                        pass
+                    #Reason of endpoint
+                    if side['result'] == 'checkmated' or opponent['result'] == 'checkmated':
+                        reason = 'checkmate'
+                    if side['result'] == 'agreed' or opponent['result'] == 'agreed':
+                        reason = 'an agreemend'
+                    if side['result'] == 'repetition' or opponent['result'] == 'repetition':
+                        reason = 'repetition'
+                    if side['result'] == 'timeout' or opponent['result'] == 'timeout':
+                        reason = 'timeout'
+                    if side['result'] == 'resigned' or opponent['result'] == 'resigned':
+                        reason = 'resignation'
+                    if side['result'] == 'stalemate' or opponent['result'] == 'stalemate':
+                        reason = 'a stalemate'
+                    if side['result'] == 'insufficient' or opponent['result'] == 'insufficient':
+                        reason = 'insufficient material'
+                    if side['result'] == '50move' or opponent['result'] == '50move':
+                        reason = 'the 50-move rule'
+                    if side['result'] == 'abandoned' or opponent['result'] == 'abandoned':
+                        reason = 'abandonment'
+                    if side['result'] == 'kingofthehill' or opponent['result'] == 'kingofthehill':
+                        reason = 'the opponent king who reached the hill'
+                    if side['result'] == 'threecheck' or opponent['result'] == 'threecheck':
+                        reason = 'three check'
+                    if side['result'] == 'timevsinsufficient' or opponent['result'] == 'timevsinsufficient':
+                        reason = 'the timeout vs insufficient material rule'
+                    if side['result'] == 'bughousepartnerlose' or opponent['result'] == 'bughousepartnerlose':
+                        reason = 'Bughouse partner lost'
+                    else:
+                        pass
+                    #Endboard image adress
+                    endboard = f'https://www.chess.com/dynboard?fen={fen}&board=brown&piece=neo&size=3'
+                    #Alert                
+                    alert = DiscordEmbed(
+                        title = f'{player} just played a game of chess!',
+                        description = f'{player} played {rated} {time_class} game and {result} by {reason}.',
+                        color = '769656'
+                    )
+                    alert.set_author(name='Chess Bot', icon_url='https://play-lh.googleusercontent.com/a7R5nyeaX8lIEWdBOxjlvbyq9LcFwh3XMvNtBPEKR3LPGgdvgGrec4sJwn8tUaaSkw')
+                    alert.add_embed_field(name='Url', value=url)
+                    alert.set_image(url=endboard)
+                    alert.set_footer(text='Made by AnanasHerz#5480')
+                    webhook.add_embed(alert)
+                    #Sending the message or not
+                    webhook.execute()
+                    print('Webhook send!')
+                    time.sleep(5)
                 if suspected_last_game == last_game:
                     time.sleep(5)
                     loop()
                 else:
                     pass
-                #Empty variables
-                rated = ''
-                time_class = ''
-                result = ''
-                reason = ''
-                side = ''
-                opponent = ''
-                endboard = ''
-                #Url var
-                url = response['games'][newest]['url']
-                #Rated or unrated
-                if response['games'][newest]['rated'] is True:
-                    rated = 'a rated'
-                if response['games'][newest]['rated'] is False:
-                    rated = 'an unrated'
-                else:
-                    pass
-                #Time class of game (Blitz, Bullet...) and url friendly endboard image adress
-                time_class = response['games'][newest]['time_class']
-                fen = response['games'][newest]['fen'].replace(" ", "%20")
-                #Defines the sides
-                if response['games'][newest]['white']['username'] == player:
-                    side = response['games'][newest]['white']
-                    opponent = response['games'][newest]['black']
-                if response['games'][newest]['black']['username'] == player:
-                    side = response['games'][newest]['black']
-                    opponent = response['games'][newest]['white']
-                if response['games'][newest]['white']['username'] != player and response['games'][newest]['black']['username'] != player:
-                    side = 'invalid colors'
-                    opponent = 'invalid colors'
-                    print('Invalid color')
-                else:
-                    pass
-                #Result codes
-                if side['result'] == 'win':
-                    result = 'won'
-                if opponent['result'] == 'win':
-                    result = 'lost'
-                if side['result'] == 'agreed' or side['result'] == 'repetition' or side['result'] == 'stalemate' or side['result'] == 'insufficient' or side['result'] == '50move' or side['result'] == 'timevsinsufficient':
-                    result = 'drawed'
-                else:
-                    pass
-                #Reason of endpoint
-                if side['result'] == 'checkmated' or opponent['result'] == 'checkmated':
-                    reason = 'checkmate'
-                if side['result'] == 'agreed' or opponent['result'] == 'agreed':
-                    reason = 'an agreemend'
-                if side['result'] == 'repetition' or opponent['result'] == 'repetition':
-                    reason = 'repetition'
-                if side['result'] == 'timeout' or opponent['result'] == 'timeout':
-                    reason = 'timeout'
-                if side['result'] == 'resigned' or opponent['result'] == 'resigned':
-                    reason = 'resignation'
-                if side['result'] == 'stalemate' or opponent['result'] == 'stalemate':
-                    reason = 'a stalemate'
-                if side['result'] == 'insufficient' or opponent['result'] == 'insufficient':
-                    reason = 'insufficient material'
-                if side['result'] == '50move' or opponent['result'] == '50move':
-                    reason = 'the 50-move rule'
-                if side['result'] == 'abandoned' or opponent['result'] == 'abandoned':
-                    reason = 'abandonment'
-                if side['result'] == 'kingofthehill' or opponent['result'] == 'kingofthehill':
-                    reason = 'the opponent king who reached the hill'
-                if side['result'] == 'threecheck' or opponent['result'] == 'threecheck':
-                    reason = 'three check'
-                if side['result'] == 'timevsinsufficient' or opponent['result'] == 'timevsinsufficient':
-                    reason = 'the timeout vs insufficient material rule'
-                if side['result'] == 'bughousepartnerlose' or opponent['result'] == 'bughousepartnerlose':
-                    reason = 'Bughouse partner lost'
-                else:
-                    pass
-                #Endboard image adress
-                endboard = f'https://www.chess.com/dynboard?fen={fen}&board=brown&piece=neo&size=3'
-                #Alert                
-                alert = DiscordEmbed(
-                    title = f'{player} just played a game of chess!',
-                    description = f'{player} played {rated} {time_class} game and {result} by {reason}.',
-                    color = '769656'
-                )
-                alert.set_author(name='Chess Bot', icon_url='https://play-lh.googleusercontent.com/a7R5nyeaX8lIEWdBOxjlvbyq9LcFwh3XMvNtBPEKR3LPGgdvgGrec4sJwn8tUaaSkw')
-                alert.add_embed_field(name='Url', value=url)
-                alert.set_image(url=endboard)
-                alert.set_footer(text='Made by AnanasHerz#5480')
-                webhook.add_embed(alert)
-                #Sending the message or not
-                if message == True:
-                    webhook.execute()
-                    print('Webhook send!')
-                    message = False
-                    time.sleep(5)
-                if message == False:
-                    time.sleep(5)
             if requests.get(most_recent_archive).status_code == 200 and archive == '[]':
                 print('Empty archive')
                 time.sleep(5)
